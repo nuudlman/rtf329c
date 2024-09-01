55f95b5be921249d0d6e6738dc164f8210750092
Isaac Nudelman <isaac.nudelman@utexas.edu>
2024-09-01 14:06:45 -0500

    Add mimalloc

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-// Bevy code commonly triggers these lints and they may be important signals
-// about code quality. They are sometimes hard to avoid though, and the CI
-// workflow treats them as errors, so this allows them throughout the project.
-// Feel free to delete this line.
-#![allow(clippy::too_many_arguments, clippy::type_complexity)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::asset::AssetMetaCheck;
-use bevy::prelude::*;
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+use bevy::{
+    prelude::*,
+    render::{settings::{Backends, WgpuSettings}, RenderPlugin}
+};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins.set(RenderPlugin {
+                render_creation: WgpuSettings {
+                    backends: Some(Backends::VULKAN), // Otherwise dx12/vulkan mix on windows causes annoying errors w/ AMD
+                    ..default()
+                }
+                    .into(),
+                ..default()
+            }),
+        )
         .add_systems(Startup, setup)
         .run();
 }
